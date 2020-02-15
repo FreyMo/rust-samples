dadfe1f248e44847c6caa4240da5d3381097d787
Moritz Freyburger <moritz@Moritzs-MacBook-Pro.local>
2020-02-15 21:12:46 +0100

    first macro and print line

--- first/src/main.rs
+++ first/src/main.rs
@@ -1,3 +1,15 @@
+
+macro_rules! my_first_macro {
+    () => {
+        println!("Hi there!");
+    };
+}
+
 fn main() {
-    println!("Hello, world!");
+    let _vector: Vec<u32> = Vec::new();
+
+    let array = vec![1, 2, 3, 4];
+
+    my_first_macro!();
+    println!("{:?}", array);
 }
