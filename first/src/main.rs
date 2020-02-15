30a4f2c2af5d8f65d771180281d7b7f797c5306a
Moritz Freyburger <moritz@Moritzs-MacBook-Pro.local>
2020-02-15 22:08:59 +0100

    Add stuff

--- first/src/main.rs
+++ first/src/main.rs
@@ -10,6 +10,27 @@ fn main() {
 
     let array = vec![1, 2, 3, 4];
 
+    match array.len() {
+        1 => println!("1"),
+        _ => println!("anything"),
+    }
+
     my_first_macro!();
     println!("{:?}", array);
+}
+
+struct MyGuy {
+    name: String,
+    age: u32
+}
+
+trait CanBePrinted {
+    fn print_me(&self);
+}
+
+impl CanBePrinted for MyGuy {
+    fn print_me(&self) {
+        println!("Name: {}", self.name);
+        println!("Age: {}", self.age);
+    }
 }
--- guessing-game/src/main.rs
+++ guessing-game/src/main.rs
@@ -1,3 +1,21 @@
+extern crate rand;
+
+use std::io;
+use rand::Rng;
+
 fn main() {
-    println!("Hello, world!");
+    println!("This is guess the number");
+    println!("Enter your guess!");
+
+    let mut guess = String::new();
+
+    let secret_number = rand::thread_rng().gen_range(1, 100);
+
+    println!("Random: {}", secret_number);
+
+    io::stdin()
+        .read_line(&mut guess)
+        .expect("Expected a result. Failed.");
+
+    println!("You guessed {}", guess);
 }
