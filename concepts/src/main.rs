95ee456e8cf441f5f6628c6d23a0f258bc2bd2aa
Moritz Freyburger <moritz@Moritzs-MacBook-Pro.local>
2020-02-19 21:49:57 +0100

    add enum, struct, derive

--- concepts/src/main.rs
+++ concepts/src/main.rs
@@ -1,26 +1,41 @@
+#[derive(Debug)]
+enum Day {
+    Monday(u32),
+    Tuesday()
+}
 
-const MY_CONST: u32 = 100_000;
+#[derive(Debug)]
+struct MyStruct {
+    day: Day,
+    night: Day 
+}
+
+impl MyStruct {
+    fn print_me(&self) -> u64 {
+        println!("{:?}", self);
+        5
+    }
+} 
 
 fn main() {
     let _x = 5;
     let _x = 6; // shadowing
-    let byte: u8 = 0b11111100;
-
-    let result = byte >> 3;
+    let _byte: u8 = 0b11111100;
 
     let tup: (u32, u32, u128) = (500, 500, 10000012387455);
     let (_a, _b, _c) = tup;
+
+    let my_struct = MyStruct { day: Day::Monday(33), night: Day::Tuesday() };
+
+    my_struct.print_me();
     println!("{}", tup.2);
 
-    allocate_memory_on_stack();
-    println!("{}", byte);
-    println!("{}", result);
-    println!("{}", MY_CONST);
-    println!("Hello, world!");
+    allocate_on_stack();
 }
 
-fn allocate_memory_on_stack() {
+fn allocate_on_stack() {
     let _another_array = [0; 1000000];
     
     println!("{}", _another_array[0]);
+    println!("{}", _another_array.len());
 }
